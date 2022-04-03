2c5bc5d1629079b14a4f61cf843fd296eca27f35
Heeun99 <yhe9910@naver.com>
2022-04-03 17:10:26 +0000

    ReplitEx-1

---
+++ repractice/13.py
@@ -0,0 +1,5 @@
+c = input()
+d = input()
+
+print(d)
+print(c)
---
+++ repractice/70.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+if a//3 == 1:
+  print("spring")
+else:
+  if a//3 == 3:
+    print("fall")
+  else:
+    if a//3 == 2:
+      print("summer")
+    else:
+      print("winter")
