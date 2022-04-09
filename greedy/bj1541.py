b1a5e10f4be3290cde90eba2b49a7f41b2f86ac8
Heeun99 <yhe9910@naver.com>
2022-04-09 17:13:51 +0000

    ReplitEx-1

--- greedy/bj1541.py
+++ greedy/bj1541.py
@@ -1,17 +1,19 @@
 n = input()
 
 sp = n.split('-')
-m = 0
 
-if sp[0].isdigit() == True:
-  m += int(sp[0])
-else:
-  m += eval(sp[0]) 
+num = []
 
-for i in range(1,len(sp)):
-  if sp[i].isdigit()==True:
-    m -= int(sp[i])
-  else:
-    m -= eval(sp[i])
+for i in sp:
+  cnt = 0
+  s = i.split('+')
+  for j in s:
+    cnt += int(j)
+  num.append(cnt)
 
-print(m)
+a = num[0]
+
+for i in range(1,len(num)):
+  a -= num[i]
+
+print(a)
