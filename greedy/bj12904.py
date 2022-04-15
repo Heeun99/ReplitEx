4774058ea6a4f467c75ddd5e0ee5c0134da24910
Heeun99 <yhe9910@naver.com>
2022-04-15 07:36:24 +0000

    ReplitEx-1

--- greedy/bj12904.py
+++ greedy/bj12904.py
@@ -3,16 +3,18 @@ t = list(input())
 res = 0
 
 for i in range(len(t)-1,-1,-1):
-
   if t == s:
     res = 1
     break
     
   if t[i] == "A":
     t.pop()
+    print(t)
     
   else:
     t.pop()
     t.reverse()
+    print(t)
+    
 
 print(res)
